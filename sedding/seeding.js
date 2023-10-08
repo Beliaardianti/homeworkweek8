c68429c52297496282f119c48e9c6f68b8cef198
Belia Ardianti <114715788+Beliaardianti@users.noreply.github.com>
2023-10-08 16:16:41 +0700

    homeworkweek8

--- sedding/seeding.js
+++ sedding/seeding.js
@@ -2,7 +2,7 @@ const pool = require('../src/queries.js')
 
 const fs = require('fs');
 
-const seedQuery = fs.readFileSync(`db/seeding.sql`, {encoding: 'utf8'});
+const seedQuery = fs.readFileSync(`sedding/seeding.sql`, {encoding: 'utf8'});
 pool.query(seedQuery,(err, res) => {
     console.log(err);
     console.log("Berhasil");
